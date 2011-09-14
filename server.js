9edd6e0ecedd0764ed3b8002356bdc35fe45299f
Mary Rose Cook <maryrosecook@maryrosecook.com>
2011-09-14 19:28:44 +0100

    heroku

--- server.js
+++ server.js
@@ -23,30 +23,30 @@ fu.get("/send_message", function (req, res) {
   var message = qs.parse(url.parse(req.url).query).message;
   storeMessage(message, function() {
     res.simpleJSON(200, {});
-		latestMessageReceived = new Date().getTime();
+	latestMessageReceived = new Date().getTime();
 
-		// respond to queued message requests from clients now that we have a new message to send out
-		while (messageRequests.length > 0)
-		{
-			var messageRequest = messageRequests.shift();
-			messageRequest.callback(messageRequest.res);
-		}
+	// respond to queued message requests from clients now that we have a new message to send out
+	while (messageRequests.length > 0)
+	{
+	  var messageRequest = messageRequests.shift();
+	  messageRequest.callback(messageRequest.res);
+	}
   });
 });
 
 // put message in list and store it in quick access latest_message var
 function storeMessage(message, callback) {
-	  redisClient.lpush('messages', message, function (err, value) {
-	    callback();
-	  });
+  redisClient.lpush('messages', message, function (err, value) {
+	callback();
+  });
 }
 
 // if new message available for client, return it immediately,
 // or queue request to be dealt with next time new message comes in
 fu.get("/latest_message", function (req, res) {
-	var since = parseInt(qs.parse(url.parse(req.url).query).since);
+  var since = parseInt(qs.parse(url.parse(req.url).query).since);
 
-	if(since < latestMessageReceived) // new message since client last requested
+  if(since < latestMessageReceived) // new message since client last requested
     sendLatestMessageToClient(res); // send it straight to them
   else
     messageRequests.push({ callback: sendLatestMessageToClient, res: res }); // queue up the requst
@@ -54,38 +54,36 @@ fu.get("/latest_message", function (req, res) {
 
 // retrieves latest message and sends it to user
 function sendLatestMessageToClient(res) {
-		redisClient.lindex('messages', 0, function (err, value) {
-			res.simpleJSON(200, { message: value.toString(), timestamp: new Date().getTime() });
-		});
+  redisClient.lindex('messages', 0, function (err, value) {
+	res.simpleJSON(200, { message: value.toString(), timestamp: new Date().getTime() });
+  });
 }
 
 // returns true if send message already stored (already said)
 fu.get("/unique_chalk", function (req, res) {
-	var new_message = qs.parse(url.parse(req.url).query).message;
+  var new_message = qs.parse(url.parse(req.url).query).message;
 
-		redisClient.lrange('messages', 0, -1, function (err, value) {
-			uniqueChalk = true;
-			for(i in value)
-				if(value[i] == new_message)
-				{
-					uniqueChalk = false;
-					break;
-				}
+  redisClient.lrange('messages', 0, -1, function (err, value) {
+	uniqueChalk = true;
+	for(i in value)
+	  if(value[i] == new_message)
+	{
+	  uniqueChalk = false;
+	  break;
+	}
 
-			res.simpleJSON(200, { message: uniqueChalk });
-		});
+	res.simpleJSON(200, { message: uniqueChalk });
+  });
 });
 
 // setup of initial message if required
-function initialSetup() {
-    	redisClient.exists('messages', function (err, value) {
-    		if(value == 0)
-    		{
-    			storeMessage("We liked the same music, we liked the same bands, we liked the same clothes.", function() {
-    		    res.simpleJSON(200, {});
-    		  });
-    		}
-    	});
+function initialSetup(res) {
+  redisClient.exists('messages', function (err, value) {
+    if(value == 0)
+    {
+      storeMessage("We liked the same music, we liked the same bands, we liked the same clothes.", function() {});
+    }
+  });
 }
 
 function makeRedisClient() {
